4071d75752227a2f3925c671a96b912c3e54629c
Andrew Gordon <andrewgordon@Omega-Mac.local>
2012-01-16 12:49:47 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KageNet::Application.config.secret_token = '642f48d48964e67d37237b29fb626c749a7a4c931edbdc31d7267d166d28774f9528f25e426e58017cdce24299b9c8a0ed28a502562e184bb641efe1346a8c1f'
